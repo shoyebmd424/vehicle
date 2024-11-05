e54321b330401aaa0cee76135b386176b3798d9a
Md Shoyeb <96253133+shoyebmd424@users.noreply.github.com>
2024-11-05 18:20:33 +0530

    Updates

--- assets/index-Bb2I0Kd_.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Od92YIMu.js";const s=()=>r.jsx("div",{children:"Buy"});export{s as default};
---
+++ assets/index-BqUJaFi5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D9iGN99O.js";const s=()=>r.jsx("div",{children:"Buy"});export{s as default};
